2a75725f0f5e5b27fe460ef5086f9454b4c23186
Stevenliyijun <stevenliyijun@163.com>
2017-02-17 07:22:33 +0800

    new Array

--- src/testExtend/Cat.java
+++ src/testExtend/Cat.java
@@ -1,5 +1,9 @@
 package testExtend;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cat extends Animal {
 	
 	private int height = 10;
@@ -11,6 +15,7 @@ public class Cat extends Animal {
 	
 	
 	int getHeight(final int c) {
+		List aa = new ArrayList();
 		System.out.println(height);
 		return height;
 	}
